8775183731b77f14b5080effaef3104e8fec545c
kmihailo <mihailo.krstic@eng.it>
2024-10-17 11:06:58 +0200

    Basic design of login and chat room page

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,8 +1,157 @@
+import { useEffect, useState } from "react"
+import { FaPeopleGroup } from "react-icons/fa6"
+import coverImage from '../assets/chat_picture.png'
+
+const roomName = 'John Doe'
+
+const msgs = [
+    {
+        id: 1,
+        user: 'John Doe',
+        content: 'This program changed my life! I learned so much and met amazing people.',
+        country: 'Sweden',
+    },
+    {
+        id: 2,
+        user: 'Jane Smith',
+        content: 'Incredible experience! The instructors were top-notch and very supportive.',
+        country: 'Canada',
+    },
+    {
+        id: 3,
+        user: 'Emily Johnson',
+        content: 'I highly recommend this course. It provided me with the skills I needed to succeed.',
+        country: 'Germany',
+    },
+    {
+        id: 1,
+        user: 'John Doe',
+        content: 'This program changed my life! I learned so much and met amazing people.',
+        country: 'Sweden',
+    },
+    {
+        id: 2,
+        user: 'Jane Smith',
+        content: 'Incredible experience! The instructors were top-notch and very supportive.',
+        country: 'Canada',
+    },
+    {
+        id: 3,
+        user: 'Emily Johnson',
+        content: 'I highly recommend this course. It provided me with the skills I needed to succeed.',
+        country: 'Germany',
+    },
+    {
+        id: 1,
+        user: 'John Doe',
+        content: 'This program changed my life! I learned so much and met amazing people.',
+        country: 'Sweden',
+    },
+    {
+        id: 2,
+        user: 'Jane Smith',
+        content: 'Incredible experience! The instructors were top-notch and very supportive.',
+        country: 'Canada',
+    },
+    {
+        id: 3,
+        user: 'Emily Johnson',
+        content: 'I highly recommend this course. It provided me with the skills I needed to succeed.',
+        country: 'Germany',
+    },
+    {
+        id: 1,
+        user: 'John Doe',
+        content: 'This program changed my life! I learned so much and met amazing people.',
+        country: 'Sweden',
+    },
+    {
+        id: 2,
+        user: 'Jane Smith',
+        content: 'Incredible experience! The instructors were top-notch and very supportive.',
+        country: 'Canada',
+    },
+    {
+        id: 3,
+        user: 'Emily Johnson',
+        content: 'I highly recommend this course. It provided me with the skills I needed to succeed.',
+        country: 'Germany',
+    },
+    {
+        id: 1,
+        user: 'John Doe',
+        content: 'This program changed my life! I learned so much and met amazing people.',
+        country: 'Sweden',
+    },
+    {
+        id: 2,
+        user: 'Jane Smith',
+        content: 'Incredible experience! The instructors were top-notch and very supportive.',
+        country: 'Canada',
+    },
+    {
+        id: 3,
+        user: 'Emily Johnson',
+        content: 'I highly recommend this course. It provided me with the skills I needed to succeed.',
+        country: 'Germany',
+    }
+]
 
 const Chat = () => {
-  return (
-    <div>Chat</div>
-  )
+    const [message, setMessage] = useState('')
+    const [messages, setMessages] = useState([])
+
+    useEffect(() => {
+        setMessages(msgs)
+    }, [])
+
+    const sendMessage = () => {
+        setMessage('')
+    }
+
+    return (
+        <div className='bg-violet-900'  style={{ backgroundImage: `url(${coverImage})` }}>
+            <div className='max-w-6xl mx-auto'>
+                <div className='flex flex-col mx-20 h-screen bg-gray-200 rounded-xl'>
+                    <div className='flex flex-row font-bold text-3xl text-blue-900 bg-gray-300 rounded-xl py-10 '>
+                        <h1 className='text-5xl pl-4' ><FaPeopleGroup /></h1>
+                        <h2 className='pl-4 '>Chat {roomName}</h2>
+                    </div>
+                    <div className='overflow-auto rounded-xl'>
+                        {messages.map((msg, index) => (
+                            <div key={index}  className='m-8'>
+                                { msg.user && msg.user === roomName ? 
+                                 <div className='flex flex-row justify-end'>
+                                    <p className='text-right font-semibold rounded-xl p-2 bg-blue-200'>{msg.content}</p>
+                                </div>
+                                 : 
+                                 <div className='flex flex-row justify-start'>
+                                    <p className='text-left font-semibold rounded-xl p-2 bg-gray-100'><strong>{msg.user}:</strong> {msg.content}</p>
+                                    </div>
+                                }
+                            </div>
+                        ))}
+                    </div>
+                    <div className='flex flex-row h-12xs pb-10 px-10'>
+                        <input
+                            id='message'
+                            type='text'
+                            name='message'
+                            value={message}
+                            className='w-full py-2 px-2 bg-gray-100 rounded-md text-black'
+                            placeholder='Enter your message here'
+                            onChange={(e) => setMessage(e.target.value)}
+                            onKeyDown={(e) => { if (e.key === 'Enter') sendMessage() }}
+                            required
+                        />
+                        <button
+                            onClick={sendMessage}
+                            className='bg-gray-300 px-8 py-2 ml-3 text-black font-bold rounded-xl'>Send</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Chat
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,66 @@
 import coverImage from '../assets/chat_picture.png'
+import logo from '../assets/chat.png'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 const Login = () => {
-  return (
-    <div className='bg-cover bg-top pt-20 pb-20 h-screen' style={{ backgroundImage: `url(${coverImage})` }}>
-        Login
-    </div>
-  )
+    const [username, setUsername] = useState('')
+    const [chatRoom, setChatRoom] = useState('')
+
+    const navigate = useNavigate()
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        navigate('/chat')
+    }
+
+    return (
+        <div className='bg-cover bg-top pt-20 pb-20 h-screen' style={{ backgroundImage: `url(${coverImage})` }}>
+            <div className='max-w-6xl mx-auto'>
+                <div className='flex flex-col items-center justify-center '>
+                    <img src={logo} alt='Logo' className='w-48 h-48 mb-8' />
+                    <section className='bg-gray-100 py-8 px-4 md:px-20 mb-4 shadow-md rounded-md border m-4 md:m-0 '>
+                        <form onSubmit={onSubmit} className='flex flex-col'>
+                            <h1 className='text-3xl pb-8'>Login</h1>
+                            <label
+                                htmlFor='chatRoom'
+                                className='text-gray-700 font-bold mb-2 mr-2'
+                            >
+                                Chat Room
+                            </label>
+                            <input
+                                type='text'
+                                id='chatRoom'
+                                name='chatRoom'
+                                className='border rounded py-2 px-3 mb-4'
+                                placeholder='Chat room name'
+                                onChange={(e) => setChatRoom(e.target.value)}
+                                required
+                            />
+                            <label
+                                htmlFor='username'
+                                className='text-gray-700 font-bold mb-2 mr-2'
+                            >
+                                Username
+                            </label>
+                            <input
+                                type='text'
+                                id='username'
+                                name='username'
+                                className='border rounded py-2 px-3 mb-4'
+                                placeholder='eg. jane sparrow'
+                                onChange={(e) => setUsername(e.target.value)}
+                                required
+                            />
+                            <button
+                                className='bg-blue-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline'
+                                type='submit' >Login</button>
+                        </form>
+                    </section>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Login
